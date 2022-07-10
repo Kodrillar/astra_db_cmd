e29c91710f4f4839d54edbc18e6ca8c24aea85bd
Kodrillar <ipadeoladavid112@gmail.com>
2022-07-10 21:47:36 +0100

    credentials implementation

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,30 @@
 import 'dart:io';
 
+import 'package:astra_db_cmd/service/database_table_service.dart';
 import 'package:colorize/colorize.dart';
 
+DatabaseTableService databaseTableService = DatabaseTableService();
 void main(List<String> arguments) {
+  Future<void> getDatabaseTable(
+      {required astraDbId,
+      required astraDbRegion,
+      required keyspace,
+      required astraDbToken}) async {
+    try {
+      final tableData = await databaseTableService.getDatabaseTable(
+        astraDbId: astraDbId,
+        astraDbRegion: astraDbRegion,
+        keyspace: keyspace,
+        astraDbToken: astraDbToken,
+      );
+      print(tableData);
+    } on SocketException {
+      print(errorMessage.red().bold());
+    } catch (ex) {
+      print(errorMessage.red().bold());
+    }
+  }
+
   Colorize welcomeMessage = Colorize('Welcome to Astra_db_cmd!🙂️\n');
   Colorize option1 = Colorize('Check tables [c]');
   Colorize option2 = Colorize('Check keyspace [ck]');
@@ -10,4 +32,20 @@ void main(List<String> arguments) {
     '\n${welcomeMessage.blue().bold()}\n${option1.yellow().bold()}\n${option2.cyan()}\n\nwhat would you like to do? ',
   );
   final line = stdin.readLineSync();
+  if (line == 'c') {
+    stdout.write('Input your AstraDB ID: ');
+    final astraDbId = stdin.readLineSync();
+    stdout.write('Input your AstraDB Region: ');
+    final astraDbRegion = stdin.readLineSync();
+    stdout.write('Input your AstraDB Token: ');
+    final astraDbToken = stdin.readLineSync();
+    stdout.write('Input your Keyspace: ');
+    final keyspace = stdin.readLineSync();
+    getDatabaseTable(
+      astraDbId: astraDbId,
+      astraDbRegion: astraDbRegion,
+      keyspace: keyspace,
+      astraDbToken: astraDbToken,
+    );
+  }
 }
--- lib/utils/astra_db_credentials.dart
+++ lib/utils/astra_db_credentials.dart
@@ -3,8 +3,10 @@ class AstraDbCredential {
     required this.astraDbId,
     required this.astraDbRegion,
     required this.keyspace,
+    required this.astraDbToken,
   });
   final String astraDbId;
   final String astraDbRegion;
   final String keyspace;
+  final String astraDbToken;
 }
